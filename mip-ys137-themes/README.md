d8a942afa0ee1aaa00185b39b18debc9b5877581
regboy8 <regboy@qq.com>
2017-05-24 11:53:23 +0800

    增加页面主题样式管理组件

---
+++ mip-ys137-themes/README.md
@@ -0,0 +1,23 @@
+# mip-ys137-themes
+
+mip-ys137-themes 页面主题样式管理
+
+标题|内容
+----|----
+类型|定制
+支持布局|responsive,fixed-height,fill,container,fixed
+所需脚本|http://mipcache.bdstatic.com/extensions/platform/v1/mip-ys137-themes/mip-ys137-themes.js
+
+## 示例
+```html
+<mip-ys137-themes>
+<mip-img
+    layout="responsive" 
+    width="350" 
+    height="263"
+    src="http://ztd00.photos.bdimg.com/ztd/w%3D350%3Bq%3D70/sign=e3bb1c4b97ef76c6d0d2fd2ead2d8cc7/f703738da9773912b57d4b0bff198618367ae205.jpg">
+</mip-img>
+</mip-ys137-themes>
+```
+
+## 属性
---
+++ mip-ys137-themes/mip-ys137-themes.js
@@ -0,0 +1,21 @@
+/**
+ * @author: mj
+ * @date:  2017-05-24
+ * @time: 14:25
+ * @file: mip-ys137-themes.js
+ * @contact: regboy@qq.com
+ * @description: 页面主题样式管理
+ */
+define(function(require) {
+    var customElem = require('customElement').create();
+	// build 方法，元素插入到文档时执行，仅会执行一次
+    customElem.prototype.build = function () {
+		console.log("build");
+    };
+    // 第一次进入可视区回调,只会执行一次，做懒加载，利于网页速度
+    customElem.prototype.firstInviewCallback = function () {
+		console.log("firstInviewCallback");
+    };
+	
+    return customElem;
+});
